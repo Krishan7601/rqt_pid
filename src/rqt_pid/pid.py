869846e97016967c657ac4da7ddcce5a35611641
Krishan Patel <krishan7603@gmail.com>
2018-03-09 12:45:22 +0000

    updated paths

--- src/rqt_pid/pid.py
+++ src/rqt_pid/pid.py
@@ -22,14 +22,14 @@ class PID(Plugin):
                       help="Put plugin in silent mode")
         args, unknowns = parser.parse_known_args(context.argv())
         if not args.quiet:
-            print 'Running rqt_pid
+            print 'Running rqt_pid'
 
         # Create QWidget
         self._widget = QWidget()
         # Get path to UI file which should be in the "resource" folder of this package
-        ui_file = os.path.join(rospkg.RosPack().get_path('rqt_drone'), 'resource', 'pid.ui')
+        ui_file = os.path.join(rospkg.RosPack().get_path('rqt_pid'), 'resource', 'pid.ui')
         # Extend the widget with all attributes and children from UI file
-        loadUi('/home/krishan/catkin_ws/src/rqt_drone/resource/pid.ui', self._widget, {'PIDView' : 'PIDView'})
+        loadUi('/home/krishan/catkin_ws/src/rqt_pid/resource/pid.ui', self._widget, {'PIDView' : 'PIDView'})
         # Give QObjects reasonable names
         self._widget.setObjectName('PID_UI')
         # Show _widget.windowTitle on left-top of each plugin (when 
